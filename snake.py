9ab4425a2de7a0ee7925cfd7c4c197d21312342e
Alan Chan <victor26537885@gmail.com>
2022-10-05 13:47:20 +0100

    added file function

--- snake.py
+++ snake.py
@@ -33,6 +33,12 @@ class Snake:
     def extend(self):
         self.add_segment(self.segments[-1].position())
 
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
 
     # TODO- 2 Move the snake
     def move(self):
